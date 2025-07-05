0170a1f437999504d09135c128cf76f8f27a2a0d
Alex691223 <oleksandrdavydovych94@gmail.com>
2025-07-05 22:35:59 +0200

    Update bot.py

--- telegram_iris_bot/bot.py
+++ telegram_iris_bot/bot.py
@@ -1,1 +1,16 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+import modules.moderation
+import modules.automod
+import modules.logging
+import modules.greetings
 
+bot = Client("IrisBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+modules.moderation.init(bot)
+modules.automod.init(bot)
+modules.logging.init(bot)
+modules.greetings.init(bot)
+
+print("✅ Бот запущен.")
+bot.run()
