339fba148a253a0ee2d47eefa5931736e41e708b
Alex691223 <oleksandrdavydovych94@gmail.com>
2025-07-05 11:15:18 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-API_ID = 123456
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
-OWNER_ID = 954426279
-TARGET_GROUP_ID = -1001234567890
-
-MONGO_URI = "mongodb://localhost:27017"
-DB_NAME = "modbot"
-ADMINS = [OWNER_ID]  # Расширяем по желанию
