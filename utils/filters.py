e27a0c13400ec0503cdf252bbe24575f631e71cf
Alex691223 <oleksandrdavydovych94@gmail.com>
2025-07-05 14:55:11 +0200

    Delete utils directory

--- utils/filters.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import filters
-from config import ADMINS
-
-def is_admin():
-    return filters.user(ADMINS)
--- utils/helpers.py
+++
@@ -1,6 +0,0 @@
-def extract_user_id(msg):
-    if msg.reply_to_message:
-        return msg.reply_to_message.from_user.id
-    elif msg.command and len(msg.command) > 1 and msg.command[1].isdigit():
-        return int(msg.command[1])
-    return None
