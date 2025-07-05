9ac62f76c6d588c897de9cb769d3a3d9df96b425
Alex691223 <oleksandrdavydovych94@gmail.com>
2025-07-05 14:55:04 +0200

    Delete handlers directory

--- handlers/pm_to_group.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-
-def register_handlers(app: Client):
-    @app.on_message(filters.private & filters.text)
-    async def reply_pm(_, msg):
-        await msg.reply("👋 Это ЛС. Добавь меня в группу, чтобы я мог модерировать.")
