2aa39c939b43cd3b8b4e56494179191c2894813d
Alex691223 <oleksandrdavydovych94@gmail.com>
2025-07-05 10:42:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-import os
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+OWNER_ID = 954426279
+TARGET_GROUP_ID = -1001234567890
 
-API_TOKEN = os.getenv("API_TOKEN")
-
-# Вставь свой Telegram ID
-ADMINS = {954426279}  # 🔁 Замените на свой
+MONGO_URI = "mongodb://localhost:27017"
+DB_NAME = "modbot"
+ADMINS = [OWNER_ID]  # Расширяем по желанию
