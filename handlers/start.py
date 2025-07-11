b60b1955b55dd8938608b4a586da8b5dc75f91b2
Alex691223 <oleksandrdavydovych94@gmail.com>
2025-07-11 12:20:34 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
