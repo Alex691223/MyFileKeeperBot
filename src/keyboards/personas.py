fe22347338f45721f14fb87723bd568397cfb18b
Alex691223 <oleksandrdavydovych94@gmail.com>
2025-07-11 21:51:01 +0200

    Delete src directory

--- src/keyboards/personas.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from services.personas import personas
-
-def persona_keyboard() -> InlineKeyboardMarkup:
-    buttons = [[InlineKeyboardButton(text=name, callback_data=f"persona:{name}")] for name in personas]
-    return InlineKeyboardMarkup(inline_keyboard=buttons)
