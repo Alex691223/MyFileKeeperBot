0ccd9d34c12c0c20d2e824424b194fd8cbdfb838
Alex691223 <oleksandrdavydovych94@gmail.com>
2025-07-12 14:31:42 +0200

    Delete utils directory

--- utils/logger.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-from config import DB_PATH
-
-def log_action(user_id, action, chat_id=None):
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    c.execute("INSERT INTO logs (user_id, action, chat_id) VALUES (?, ?, ?)", (user_id, action, chat_id))
-    conn.commit()
-    conn.close()
